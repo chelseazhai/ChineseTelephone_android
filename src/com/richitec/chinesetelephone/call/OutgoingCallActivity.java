b4027e67f1e5cc15d7c6efddc57a51b0f2e67923
chelsea zhai <chelsea.zhai@gmail.com>
2012-10-22 18:33:27 +0800

    add outgoing call activity

---
+++ src/com/richitec/chinesetelephone/call/OutgoingCallActivity.java
@@ -0,0 +1,18 @@
+package com.richitec.chinesetelephone.call;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.richitec.chinesetelephone.R;
+
+public class OutgoingCallActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// set content view
+		setContentView(R.layout.outgoing_call_activity_layout);
+	}
+
+}
