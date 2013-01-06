c445941e2ddc5db012fe0228e0df1c8cc8e34eda
chelsea zhai <chelsea.zhai@gmail.com>
2013-01-06 13:00:27 +0800

    tmp saved

--- src/com/richitec/chinesetelephone/tab7tabcontent/CallRecordDetailInfoActivity.java
+++ src/com/richitec/chinesetelephone/tab7tabcontent/CallRecordDetailInfoActivity.java
@@ -10,11 +10,16 @@ import java.util.Map;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.richitec.chinesetelephone.R;
+import com.richitec.chinesetelephone.sip.SipCallMode;
+import com.richitec.chinesetelephone.sip.SipUtils;
 import com.richitec.commontoolkit.activityextension.NavigationActivity;
 import com.richitec.commontoolkit.calllog.CallLogBean;
 import com.richitec.commontoolkit.calllog.CallLogBean.CallType;
@@ -129,14 +134,18 @@ public class CallRecordDetailInfoActivity extends NavigationActivity {
 				// get call record detail info operation listView
 				ListView _callRecordDetailInfoOperationListView = (ListView) findViewById(R.id.callRecord_detailInfo_operation_listView);
 
-				// check callee phone and set operation listView adapter
+				// check callee phone
 				if (null == _calleePhone
 						|| _calleePhone.trim().equalsIgnoreCase("")) {
 					Log.d(LOG_TAG, "Call record detail info operation phone = "
 							+ _calleePhone);
 				} else {
+					// set operation listView adapter and on item click listener
 					_callRecordDetailInfoOperationListView
 							.setAdapter(generateCallRecordDetailInfoOperationAdapter(_calleePhone));
+
+					_callRecordDetailInfoOperationListView
+							.setOnItemClickListener(new CallRecordDetailInfoOperationListViewOnItemClickListener());
 				}
 			} else {
 				Log.e(LOG_TAG, "Get call log error, call log bean is null");
@@ -179,6 +188,38 @@ public class CallRecordDetailInfoActivity extends NavigationActivity {
 				new String[] { OPERATION_TIP, OPERATION_PHONE }, new int[] {
 						R.id.callRecord_detailInfo_operationTip_textView,
 						R.id.callRecord_detailInfo_operationPhone_textView });
+	}
+
+	// inner class
+	// call record detail info operation listView on item click listener
+	class CallRecordDetailInfoOperationListViewOnItemClickListener implements
+			OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			// check clicked position
+			switch (position) {
+			case 0:
+				// direct call
+				SipUtils.makeSipVoiceCall(_mCalleeName, _mCalleePhone,
+						SipCallMode.DIRECT_CALL);
+				break;
+
+			case 1:
+				// call back
+				SipUtils.makeSipVoiceCall(_mCalleeName, _mCalleePhone,
+						SipCallMode.CALLBACK);
+				break;
+
+			case 2:
+			default:
+				// send short message
+				Log.d(LOG_TAG, "Send short message not implement");
+				break;
+			}
+		}
+
 	}
 
 }
