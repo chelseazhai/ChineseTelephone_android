7895a3abe71b5ec3af948a988670d568e37b0aec
chelsea zhai <chelsea.zhai@gmail.com>
2013-01-25 20:12:57 +0800

    modify contacts phone number select popup window dismiss using animation

--- src/com/richitec/chinesetelephone/tab7tabcontent/ContactListTabContentActivity.java
+++ src/com/richitec/chinesetelephone/tab7tabcontent/ContactListTabContentActivity.java
@@ -744,7 +744,7 @@ public class ContactListTabContentActivity extends NavigationActivity {
 			@Override
 			public void onClick(View v) {
 				// dismiss contact phone select popup window with animation
-				dismiss();
+				dismissWithAnimation();
 			}
 
 		}
